6a0de06d880f81398156b750e5891cf981146960
enricorenna01 <erenna39@gmail.com>
2025-02-04 18:15:55 +0100

    feat: added sum

--- src/org/lessons/java/utils/CalcoliHelper.java
+++ src/org/lessons/java/utils/CalcoliHelper.java
@@ -1,5 +1,19 @@
 package org.lessons.java.utils;
 
 public class CalcoliHelper {
+    
+    //Costruttore privato
+    private CalcoliHelper(){
+    
+    }
 
+    //Somma interi
+    public static int somma(int a, int b){
+        return a + b;
+    }
+
+    //Somma double
+    public static double somma(double a, double b){
+        return a + b;
+    }
 }
